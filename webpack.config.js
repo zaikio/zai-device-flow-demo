7c781b1fc50e97d8ee0fe0908b6adf00253b3107
Jalyna <jalyna.schroeder@gmail.com>
2019-11-19 16:43:06 +0100

    Use DefinePlugin for production to pass env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
 
 let config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/assets'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist/assets")
   },
 
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    publicPath: '/assets/',
+    contentBase: path.join(__dirname, "dist"),
+    publicPath: "/assets/",
     open: true,
     historyApiFallback: true
   }
 };
 
-if (process.env.NODE_ENV === 'development') {
-  config.plugins = [new Dotenv()]
+if (process.env.NODE_ENV === "development") {
+  config.plugins = [new Dotenv()];
+} else {
+  config.plugins = [
+    new webpack.DefinePlugin({
+      DIRECTORY_OAUTH_CLIENT_ID: process.env.DIRECTORY_OAUTH_CLIENT_ID,
+      DIRECTORY_HOST: process.env.DIRECTORY_HOST
+    })
+  ];
 }
 
 module.exports = config;
